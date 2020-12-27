c2dee52e098a2463942d6778342d4f17b4068c9c
Eugene Teoh <eugenetwc1@gmail.com>
2020-12-27 19:02:30 +0000

    Birthday part done

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,32 @@
+// Name
 let nameInput = document.getElementById('name');
 let nameOutput = document.getElementById('nameOutput')
 
 nameInput.onfocus = function() {
-    nameOutput.innerHTML = 'Hello there! What\'s your name?';
+    nameOutput.textContent = 'Hello there! What\'s your name?';
 }
 
 nameInput.onblur = function() {
     if (nameInput.value) {
-        nameOutput.innerHTML = `Hi, ${nameInput.value}!`;
+        nameOutput.textContent = `Hi, ${nameInput.value}!`;
     } 
+}
+
+// Birthday
+let ageInput = document.getElementById('date');
+let ageOutput = document.getElementById('ageOutput');
+
+ageInput.onfocus = function() {
+    ageOutput.textContent = 'Lemme guess, your age is...';
+}
+
+ageInput.onblur = function() {
+    if (ageInput.value) {
+        let birthday = new Date(ageInput.value)
+        let now = new Date();
+        let age = now.getFullYear() - birthday.getFullYear();
+        ageOutput.textContent = `Your age is ${age} years old!`;
+    } else {
+        ageOutput.textContent = 'Would you mind entering your birthday?';
+    }
 }
